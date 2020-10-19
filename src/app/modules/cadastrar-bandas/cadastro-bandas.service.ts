e99b589e04a2e3eb3e076f9b461810f282785777
Bruno Mattge <brmattge@gmail.com>
2020-10-19 00:40:09 -0300

    Implementa Get e inicia Put no cadastro

--- src/app/modules/cadastrar-bandas/cadastro-bandas.service.ts
+++ src/app/modules/cadastrar-bandas/cadastro-bandas.service.ts
@@ -1,10 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Bandas } from './cadastro-bandas.model';
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Bandas } from './cadastro-bandas.model'
 
 @Injectable()
 export class CadastroBandasService {
 
-    constructor() {}
-    
+  url = 'https://localhost:5001/api/CadastroBandas';
+
+  constructor(private http: HttpClient) { }
+
+  obterTodasBandas(): Observable<Bandas[]> {  
+    return this.http.get<Bandas[]>(`${this.url}`);  
+  }  
+
+  adicionarBanda(aluno: Bandas): Observable<Bandas> {  
+    return this.http.post<Bandas>(`${this.url}`, aluno);  
+  }  
 }
